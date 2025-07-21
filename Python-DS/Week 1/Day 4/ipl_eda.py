c2f96e630559ed1a431267a69680def1097e0fbc
Jaldhi Shukla <jaldhishukla7@gmail.com>
2025-07-21 17:22:50 +0530

    Day 5: Extended IPL Dataset - EDA & Data Visualization

--- Python-DS/Week 1/Day 4/ipl_eda.py
+++ Python-DS/Week 1/Day 4/ipl_eda.py
@@ -22,5 +22,49 @@ print(df.describe())
 # 3. Basic Insights
 # ------------------------------
 
-# top_batsman = df.groupby('batter')['batsman_runs'].sum().sort_values(ascending=False).head()
-# print("Top 10 Run Scorers:\n", top_batsman)
+# Get the top 10 run scorers
+top_batsman = df.groupby('batter')['batsman_runs'].sum().sort_values(ascending=False).head(10) # type: ignore
+print("Top 10 Run Scorers:\n", top_batsman)
+
+# Get the top 10 batsman with most fours
+fours = df[df['batsman_runs'] == 4].groupby('batter').size().sort_values(ascending=False).head(10) # type: ignore
+print("Top 10 Players with most fours:\n", fours)
+
+# Get the top 10 batsman with most sixes
+sixes = df[df['batsman_runs'] == 6].groupby('batter').size().sort_values(ascending=False).head(10) #type: ignore
+print("Top 10 Players with most sixes:\n", sixes)
+
+# Get the top 10 bowlers with most wickets
+wickets = df[df['is_wicket'] == 1].groupby('bowler').size().sort_values(ascending=False).head(10) #type: ignore
+print("Top 10 Bowlers with most wickets:\n", wickets)
+
+# Most Dismissal types
+dismissals = df['dismissal_kind'].value_counts()
+print("Dismissal by type:\n", dismissals)
+
+# Total Runs by each team
+team_runs = df.groupby('batting_team')['total_runs'].sum().sort_values(ascending=False) # type: ignore
+print("Total Runs by each team:\n", team_runs)
+
+
+# 4. Data Visualization
+# -----------------------------
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(10,6))
+sns.barplot(x=top_batsman.values, y=top_batsman.index, palette="viridis")
+plt.title("Top 10 Run Scorers in IPL")
+plt.xlabel("Total Runs")
+plt.ylabel("Batsman")
+plt.tight_layout()
+plt.show()
+
+plt.figure(figsize=(10,6))
+sns.barplot(x=wickets.values, y=wickets.index, palette="rocket")
+plt.title("Top 10 Wicket Takers in IPL")
+plt.xlabel("Wickets Taken")
+plt.ylabel("Bowler")
+plt.tight_layout()
+plt.show()
